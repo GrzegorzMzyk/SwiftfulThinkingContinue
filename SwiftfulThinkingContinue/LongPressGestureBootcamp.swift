baaf04c16d708d573608eddf0acb000f416fbe82
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-08-13 18:36:28 +0200

    Start project

---
+++ SwiftfulThinkingContinue/LongPressGestureBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  LongPressGestureBootcamp.swift
+//  SwiftfulThinkingContinue
+//
+//  Created by Grzegorz Mzyk on 13/08/2024.
+//
+
+import SwiftUI
+
+struct LongPressGestureBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LongPressGestureBootcamp()
+}
