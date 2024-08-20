d48bc1af0a96164bfbada8e8fb7e1e039d9ca01a
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-08-20 22:09:03 +0200

    Add scale and rotation effect

--- SwiftfulThinkingContinue/DragGesture.swift
+++ SwiftfulThinkingContinue/DragGesture.swift
@@ -13,22 +13,47 @@ struct DragGestureView: View {
     
     var body: some View {
         
-        RoundedRectangle(cornerRadius: 20)
-            .frame(width: 100, height: 100)
-            .offset(offset)
-            .gesture(
-                DragGesture()
-                    .onChanged({ value in
-                        withAnimation(.spring()){
-                            offset = value.translation
+        ZStack {
+            VStack{
+                Text("\(offset.width)")
+                Spacer()
+            }
+            RoundedRectangle(cornerRadius: 20)
+                .frame(width: 300, height: 500)
+                .offset(offset)
+                .scaleEffect(getScaleAmount())
+                .rotationEffect(Angle(degrees: getRotationAmount()))
+                .gesture(
+                    DragGesture()
+                        .onChanged{ value in
+                            withAnimation(.spring()){
+                                offset = value.translation
+                            }
                         }
-                    })
-                    .onEnded({ value in
-                        withAnimation(.spring()){
-                            offset = .zero
+                        .onEnded{ value in
+                            withAnimation(.spring()){
+                                offset = .zero
+                                //                            offset = CGSize(width: 0, height: 0)
+                            }
                         }
-                    })
                 )
+        }
+    }
+    
+    func getScaleAmount() -> CGFloat {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = abs(offset.width)
+        let percentage = currentAmount / max
+        return 1.0 - min(percentage, 0.5) * 0.5
+    }
+    
+    func getRotationAmount() -> Double {
+        let max = UIScreen.main.bounds.width / 2
+        let currentAmount = (offset.width)
+        let percentage = currentAmount / max
+        let percentageAsDouble = Double(percentage)
+        let maxAngle: Double = 10
+        return percentageAsDouble * maxAngle
     }
 }
 #Preview {
--- SwiftfulThinkingContinue/SwiftfulThinkingContinueApp.swift
+++ SwiftfulThinkingContinue/SwiftfulThinkingContinueApp.swift
@@ -25,7 +25,7 @@ struct SwiftfulThinkingContinueApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            DragGestureView()
         }
         .modelContainer(sharedModelContainer)
     }
