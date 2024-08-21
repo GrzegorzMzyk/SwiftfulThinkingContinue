19d1fccc4b9ed42a077fa160b84d42c5506bdf89
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-08-21 14:41:18 +0200

    geometryReader V2

--- SwiftfulThinkingContinue/GeometryReaderBootcamp.swift
+++ SwiftfulThinkingContinue/GeometryReaderBootcamp.swift
@@ -8,18 +8,43 @@
 import SwiftUI
 
 struct GeometryReaderBootcamp: View {
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.width / 2
+        let currentX = geo.frame(in: .global).midX
+        return (1 - (currentX / maxDistance))
+    }
+    
+    
     var body: some View {
-        GeometryReader { geometry in
-            HStack(spacing:0) {
-                Rectangle()
-                    .fill(Color.red)
-                    .frame(width: geometry.size.width * 0.666)
-                //                .frame(width: UIScreen.main.bounds.width * 0.666)
-                Rectangle()
-                    .fill(Color.blue)
+        ScrollView(.horizontal) {
+            HStack{
+                ForEach(0..<20) { index in
+                    GeometryReader { geometry in
+                        RoundedRectangle(cornerRadius: 20)
+                            .rotation3DEffect(
+                                Angle(degrees: getPercentage(geo: geometry) * 30),
+                                axis: (x: 0.0, y: 1.0, z: 0.0)
+                            )
+                    }
+                    .frame(width: 300, height: 250)
+                    .padding()
+                }
             }
         }
-        .ignoresSafeArea()
+        
+        
+//        GeometryReader { geometry in
+//            HStack(spacing:0) {
+//                Rectangle()
+//                    .fill(Color.red)
+//                    .frame(width: geometry.size.width * 0.666)
+//                //                .frame(width: UIScreen.main.bounds.width * 0.666)
+//                Rectangle()
+//                    .fill(Color.blue)
+//            }
+//        }
+//        .ignoresSafeArea()
     }
 }
 
