4a452e67d8f6c5e71fb99774541bcf068ba98523
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-08-27 10:35:19 +0200

    save entity

--- SwiftfulThinkingContinue/CoreDataBootcamp.swift
+++ SwiftfulThinkingContinue/CoreDataBootcamp.swift
@@ -17,13 +17,26 @@ class CoreDataViewModel: ObservableObject {
     
     // loading CoreData
     let container: NSPersistentContainer
+    @Published var saveEntitis: [FruitEntity] = []
+    
     init() {
         container = NSPersistentContainer(name: "FruitsContainer")
         container.loadPersistentStores { (description, error) in
             if let error = error {
                 print("Error loading core data \(error)")
+            } else {
+                print ("Succcesfullyy loaded core data")
             }
         }
+    }
+    func fetchFruits() {
+        let request = NSFetchRequest<FruitEntity>(entityName: "FruitEntity")
+        do {
+            saveEntitis = try container.viewContext.fetch(request)
+        } catch let error {
+            print("Error fetching \(error)")
+        }
+        
     }
 }
 
