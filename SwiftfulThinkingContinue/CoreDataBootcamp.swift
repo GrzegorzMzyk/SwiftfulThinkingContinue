82b2bd7ccc5c4d37da0fc0bb1793985a997fdcc6
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-08-29 17:26:37 +0200

    update/delete

--- SwiftfulThinkingContinue/CoreDataBootcamp.swift
+++ SwiftfulThinkingContinue/CoreDataBootcamp.swift
@@ -42,6 +42,21 @@ class CoreDataViewModel: ObservableObject {
         newFruit.name = text
         saveData()
     }
+    
+    func updateFruit(entity: FruitEntity) {
+        let currentName = entity.name ?? ""
+        let newName = currentName + "!"
+        entity.name = newName
+        saveData()
+    }
+    
+    func deleteFruit(indexSet: IndexSet) {
+        guard let index = indexSet.first else { return }
+        let entity = saveEntitis[index]
+        container.viewContext.delete(entity)
+        saveData()
+    }
+    
     func saveData() {
         do {
            try container.viewContext.save()
@@ -53,14 +68,56 @@ class CoreDataViewModel: ObservableObject {
     }
 }
 
+
+
+
+
 struct CoreDataBootcamp: View {
     
     @StateObject var vm = CoreDataViewModel()
+    @State var textFieldText: String = ""
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack(spacing:20) {
+                TextField("Add fruit here...", text: $textFieldText)
+                    .font(.headline)
+                    .padding(.leading)
+                    .frame(height: 55)
+                    .background(Color(#colorLiteral(red: 0.7540688515, green: 0.7540867925, blue: 0.7540771365, alpha: 1)))
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .padding(.horizontal)
+                Button(action: {
+                    guard !textFieldText.isEmpty else { return }
+                    vm.addFruit(text: textFieldText)
+                    textFieldText = ""
+                }, label: {
+                    Text("SAVE")
+                        .font(.headline)
+                        .foregroundStyle(Color.white)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color(#colorLiteral(red: 0.3718943, green: 0.3860599697, blue: 0.8404720426, alpha: 0.7855960265)))
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                        .padding(.horizontal)
+                })
+                List{
+                    ForEach(vm.saveEntitis) { entity in
+                        Text(entity.name ?? "No name")
+                            .onTapGesture {
+                                vm.updateFruit(entity: entity)
+                            }
+                    }
+                    .onDelete(perform: vm.deleteFruit)
+                    }
+                .listStyle(PlainListStyle())
+                }
+               
+                .navigationTitle("Fruits")
+            }
+            
+        }
     }
-}
 
 #Preview {
     CoreDataBootcamp()
