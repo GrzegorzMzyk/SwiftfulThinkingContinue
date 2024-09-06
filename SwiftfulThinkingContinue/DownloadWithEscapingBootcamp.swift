6d9f6e77d5a5e3ffc91a37d1673eb47f1f0284e9
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-06 19:01:05 +0200

    default function fot download from url

--- SwiftfulThinkingContinue/DownloadWithEscapingBootcamp.swift
+++ SwiftfulThinkingContinue/DownloadWithEscapingBootcamp.swift
@@ -7,39 +7,50 @@
 
 import SwiftUI
 
+struct PostModel :Identifiable, Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
 class DownloadWithEscapingViewModel: ObservableObject {
-    
+    @Published var posts: [PostModel] = []
     init() {
         getPosts()
     }
     
     func getPosts() {
         guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts/1") else { return }
+        downloadData(fromURL: url) { returnedData in
+            if let data = returnedData {
+                guard let newPost = try? JSONDecoder().decode(PostModel.self, from: data) else { return }
+                DispatchQueue.main.async { [weak self] in
+                    self?.posts.append(newPost)
+                }
+            } else {
+                print("no data returned")
+            }
+        }
+    }
+    // generic Dafault function to download data
+    
+    func downloadData(fromURL url: URL,completionHandler: @escaping(_ data: Data?) -> ())  {
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else {
-                print("no data")
+            guard
+                let data = data,
+                error == nil,
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300
+            else {
+                print("error download data")
+                completionHandler(nil)
                 return
             }
-            guard error == nil else {
-                print("Error:\(String(describing: error))")
-                return
-            }
-            guard let response = response as? HTTPURLResponse else {
-                print("Invalid response")
-                return
-            }
-            guard response.statusCode >= 200 && response.statusCode < 300 else {
-                print("Status code should be 2xx, but is \(response.statusCode)")
-                return
-            }
-            print("Successfully downloaded data")
-            print (data)
-            let jsonString = String(data: data, encoding: .utf8)
-            print(jsonString)
-            
+            completionHandler(data)
+           
         }.resume()
     }
-    
     
 }
 
@@ -48,7 +59,17 @@ struct DownloadWithEscapingBootcamp: View {
     @StateObject var vm = DownloadWithEscapingViewModel()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(vm.posts) { post in
+                VStack(alignment: .leading) {
+                    Text(post.title)
+                        .font(.headline)
+                    Text(post.body)
+                        .foregroundStyle(Color.gray)
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+            }
+        }
     }
 }
 
