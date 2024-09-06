addfad186aa135107bb89b1f589f9a6f0e364f15
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-06 16:30:17 +0200

    codable = encodable + decodable

--- SwiftfulThinkingContinue/CodableBootcamp.swift
+++ SwiftfulThinkingContinue/CodableBootcamp.swift
@@ -7,11 +7,43 @@
 
 import SwiftUI
 
-struct CustomerModel: Identifiable {
+// Codable = Decodable + Encodable
+
+struct CustomerModel: Identifiable, Codable { /*Decodable, Encodable*/
     let id: String
     let name: String
     let points: Int
     let isPremium: Bool
+    
+//    init(id: String, name: String, points: Int, isPremium: Bool) {
+//        self.id = id
+//        self.name = name
+//        self.points = points
+//        self.isPremium = isPremium
+//    }
+//    
+//    enum CodingKeys: String, CodingKey {
+//        case id
+//        case name
+//        case points
+//        case isPremium
+//    }
+//    // Decodable from json
+//    init(from decoder: any Decoder) throws {
+//        let container = try decoder.container(keyedBy: CodingKeys.self)
+//        self.id = try container.decode(String.self, forKey: .id)
+//        self.name = try container.decode(String.self, forKey: .name)
+//        self.points = try container.decode(Int.self, forKey: .points)
+//        self.isPremium = try container.decode(Bool.self, forKey: .isPremium)
+//    }
+//    // Encodable to json
+//    func encode(to encoder: any Encoder) throws {
+//        var container = encoder.container(keyedBy: CodingKeys.self)
+//        try container.encode(self.id, forKey: .id)
+//        try container.encode(self.name, forKey: .name)
+//        try container.encode(self.points, forKey: .points)
+//        try container.encode(self.isPremium, forKey: .isPremium)
+//    }
 }
 
 class CodableViewModel: ObservableObject {
@@ -21,41 +53,40 @@ class CodableViewModel: ObservableObject {
         getData()
     }
     
-    
-    // manual decoding
     func getData() {
         guard let data = getJSONData() else { return }
-        if
-            let localData = try? JSONSerialization.jsonObject(with: data),
-            let dictionary = localData as? [String:Any],
-            
-                let id = dictionary["id"] as? String,
-                let name = dictionary["name"] as? String,
-                let points = dictionary["points"] as? Int,
-                let isPremium = dictionary["isPremium"] as? Bool {
-            
-            let newCustomer = CustomerModel(id: id, name: name, points: points, isPremium: isPremium)
-            customer = newCustomer
-            }
-    }
+        self.customer = try? JSONDecoder().decode(CustomerModel.self, from: data)
         
-//        print("JSON Data:")
-//        print(data)
-//        let jsonString = String(data: data, encoding: .utf8)
-//        print(jsonString)
+        // manual decoding
+        
+//        if
+//            let localData = try? JSONSerialization.jsonObject(with: data),
+//            let dictionary = localData as? [String:Any],
+//            
+//                let id = dictionary["id"] as? String,
+//                let name = dictionary["name"] as? String,
+//                let points = dictionary["points"] as? Int,
+//                let isPremium = dictionary["isPremium"] as? Bool {
+//            
+//            let newCustomer = CustomerModel(id: id, name: name, points: points, isPremium: isPremium)
+//            customer = newCustomer
+//            }
+    }
     
     
     // fake jsondata
     func getJSONData() -> Data? {
+        let customer = CustomerModel(id: "111", name: "Emilly", points: 432, isPremium: true)
+        let jsonData = try? JSONEncoder().encode(customer)
         
-        let dictionary: [String:Any] = [
-            "id"   : "12345",
-            "name" : "Grzegorz",
-            "points" : 10,
-            "isPremium" : true
-        ]
-        
-        let jsonData = try? JSONSerialization.data(withJSONObject: dictionary)
+//        let dictionary: [String:Any] = [
+//            "id"   : "12345",
+//            "name" : "Grzegorz",
+//            "points" : 10,
+//            "isPremium" : true
+//        ]
+//        
+//        let jsonData = try? JSONSerialization.data(withJSONObject: dictionary)
         return jsonData
     }
 }
