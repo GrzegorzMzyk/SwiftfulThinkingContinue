743f97bb824e1b1c8482c69fce8df6f49786734b
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-06 18:29:11 +0200

    download data

--- SwiftfulThinkingContinue/SwiftfulThinkingContinueApp.swift
+++ SwiftfulThinkingContinue/SwiftfulThinkingContinueApp.swift
@@ -25,7 +25,7 @@ struct SwiftfulThinkingContinueApp: App {
 
     var body: some Scene {
         WindowGroup {
-            CodableBootcamp()
+            DownloadWithEscapingBootcamp()
         }
         .modelContainer(sharedModelContainer)
     }
