48c89fb7f2f4651ccd0448f48e9aa04dfbe38015
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-06 14:48:50 +0200

    codable

---
+++ SwiftfulThinkingContinue/CodableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CodableBootcamp.swift
+//  SwiftfulThinkingContinue
+//
+//  Created by Grzegorz Mzyk on 06/09/2024.
+//
+
+import SwiftUI
+
+struct CodableBootcamp: View {
+    var body: some View {
+        Text("Hello, World!!!")
+    }
+}
+
+#Preview {
+    CodableBootcamp()
+}
