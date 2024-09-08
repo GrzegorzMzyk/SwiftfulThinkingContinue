043756ba50cc8d7a58296113b1b4234fcb0c8fe7
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-08 20:41:54 +0200

    countdown

--- SwiftfulThinkingContinue/TimerBootcamp.swift
+++ SwiftfulThinkingContinue/TimerBootcamp.swift
@@ -12,7 +12,7 @@ struct TimerBootcamp: View {
     let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
     
     // Current Time
-    
+    /*
     @State var currentDate: Date = Date()
     var dateFormatter: DateFormatter {
         let formatter = DateFormatter()
@@ -20,6 +20,12 @@ struct TimerBootcamp: View {
         formatter.timeStyle = .medium
         return formatter
     }
+*/
+    
+    //Countdown
+    
+    @State var count: Int = 10
+    @State var finishedText: String? = nil
     
     var body: some View {
         ZStack {
@@ -29,15 +35,19 @@ struct TimerBootcamp: View {
                          endRadius: 500)
                 .ignoresSafeArea()
             
-            Text(dateFormatter.string(from: currentDate))
+            Text(finishedText ?? "\(count)")
                 .font(.system(size: 100, weight: .semibold, design: .rounded))
                 .foregroundStyle(Color.white)
                 .lineLimit(1)
                 .minimumScaleFactor(0.1)
         }
         // przyjmujemy wartość
-        .onReceive(timer, perform: { value in
-            currentDate = value
+        .onReceive(timer, perform: { _ in
+            if count <= 1 {
+                finishedText = "WOW!"
+            } else {
+                count -= 1
+            }
         })
     }
 }
