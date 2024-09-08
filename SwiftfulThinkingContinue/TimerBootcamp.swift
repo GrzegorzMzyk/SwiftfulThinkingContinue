5d74ca5ae204724433b31bc3d538035d377d0e4f
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-08 21:17:27 +0200

    Animation counter

--- SwiftfulThinkingContinue/TimerBootcamp.swift
+++ SwiftfulThinkingContinue/TimerBootcamp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct TimerBootcamp: View {
     // publikujemy wartość co jedna sekunde
-    let timer = Timer.publish(every: 1.0, on: .main, in: .common).autoconnect()
+    let timer = Timer.publish(every: 3.0, on: .main, in: .common).autoconnect()
     
     // Current Time
     /*
@@ -29,7 +29,7 @@ struct TimerBootcamp: View {
 */
     
     // Countdown to date
-    
+    /*
     @State var timeRemaining: String = ""
     let futureDate: Date = Calendar.current.date(byAdding: .hour, value: 1, to: Date()) ?? Date()
     
@@ -39,8 +39,10 @@ struct TimerBootcamp: View {
         let second = remaining.second ?? 0
         timeRemaining = "\(minute) minutes and \(second) seconds"
     }
+*/
     
-    
+    // Animation counter
+    @State var count: Int = 1
     var body: some View {
         ZStack {
           RadialGradient(colors: [Color(#colorLiteral(red: 0, green: 0.5690457821, blue: 0.5746168494, alpha: 1)), Color(#colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1))],
@@ -49,15 +51,46 @@ struct TimerBootcamp: View {
                          endRadius: 500)
                 .ignoresSafeArea()
             
-            Text(timeRemaining)
-                .font(.system(size: 100, weight: .semibold, design: .rounded))
-                .foregroundStyle(Color.white)
-                .lineLimit(1)
-                .minimumScaleFactor(0.1)
+            TabView(selection: $count,
+                    content:  {
+                Rectangle()
+                    .foregroundStyle(Color.red)
+                    .tag(1)
+                Rectangle()
+                    .foregroundStyle(Color.blue)
+                    .tag(2)
+                Rectangle()
+                    .foregroundStyle(Color.green)
+                    .tag(3)
+                Rectangle()
+                    .foregroundStyle(Color.orange)
+                    .tag(4)
+                Rectangle()
+                    .foregroundStyle(Color.yellow)
+                    .tag(5)
+                
+            })
+            .frame(height: 200)
+            .tabViewStyle(PageTabViewStyle())
+            
+//            HStack(spacing: 15) {
+//                Circle()
+//                    .offset(y: count == 1 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 2 ? -20 : 0)
+//                Circle()
+//                    .offset(y: count == 3 ? -20 : 0)
+//            }
+//            .frame(width: 150)
+//            .foregroundStyle(Color.white)
         }
         // przyjmujemy wartość
         .onReceive(timer, perform: { _ in
-            updateTimeRemaining()
+            withAnimation(.default){
+                count = count == 5 ? 1 : count + 1
+            }
+            
+            
         })
     }
 }
