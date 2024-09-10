3547e8a73611378b30e337016fdd3a687c8910b6
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-10 10:10:43 +0200

    publishers and subscribers cancellables

---
+++ SwiftfulThinkingContinue/SubscriberBootcamp.swift
@@ -0,0 +1,56 @@
+//
+//  SubscriberBootcamp.swift
+//  SwiftfulThinkingContinue
+//
+//  Created by Grzegorz Mzyk on 08/09/2024.
+//
+
+import SwiftUI
+import Combine
+
+class SubscriberViewModel: ObservableObject {
+    
+    @Published var count: Int = 0
+   
+    var cancellables = Set<AnyCancellable>()
+ 
+    init() {
+        setUpTimer()
+    }
+    
+    func setUpTimer() {
+     Timer
+            .publish(every: 1, on: .main, in: .common)
+            .autoconnect()
+            .sink { [weak self] _ in
+                guard let self = self else { return }
+                
+                self.count += 1
+                
+                if self.count >= 10 {
+                    for item in self.cancellables {
+                        item.cancel()
+                    }
+                }
+            }
+            .store(in: &cancellables)
+    }
+    
+    
+}
+
+struct SubscriberBootcamp: View {
+    
+    @StateObject var vm = SubscriberViewModel()
+    
+    var body: some View {
+        VStack {
+            Text("\(vm.count)")
+                .font(.largeTitle)
+        }
+    }
+}
+
+#Preview {
+    SubscriberBootcamp()
+}
