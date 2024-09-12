50769509f8ee69ce880bf1a0ed5d3812b2825416
GrzegorzMzyk <mzykgrzegorz@gmail.com>
2024-09-12 10:58:46 +0200

    delete image from FM

--- SwiftfulThinkingContinue/FileManagerBootcamp.swift
+++ SwiftfulThinkingContinue/FileManagerBootcamp.swift
@@ -10,12 +10,11 @@ import SwiftUI
 class LocalFileManager {
     static let instance = LocalFileManager()
     
-    func saveImage(image: UIImage, name: String) {
+    func saveImage(image: UIImage, name: String) -> String {
         guard
             let data = image.jpegData(compressionQuality: 1.0),
             let path = gethPathForImage(name: name) else {
-            print("Error getting data")
-            return
+           return "Error getting data"
         }
 //      //  let directory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
 //        let directory = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first
@@ -24,9 +23,9 @@ class LocalFileManager {
     
         do {
          try   data.write(to: path)
-            print("Succes saving")
+            return "Succes saving"
         } catch let error {
-            print ("error saving: \(error)")
+            return "error saving: \(error)"
         }
         
     }
@@ -39,6 +38,22 @@ class LocalFileManager {
         }
         return UIImage(contentsOfFile: path)
     }
+    
+    func deleteImage(name: String) -> String {
+        guard
+            let path = gethPathForImage(name: name),
+            FileManager.default.fileExists(atPath: path.path) else {
+           return "Error getting path"
+            
+        }
+        do {
+            try FileManager.default.removeItem(at: path)
+            return "sucessfully deleted"
+        } catch let error {
+            return"error deleting image\(error)"
+        }
+    }
+    
     func gethPathForImage(name: String) -> URL? {
         guard
             let path = FileManager
@@ -58,10 +73,11 @@ class FileManagerViewModel: ObservableObject {
     @Published var image: UIImage? = nil
     let imageName: String = "dog-shiba"
     let manager = LocalFileManager.instance
-    
+   @Published var infoMessage: String = ""
+   
     init() {
-      //  getImageFromAssetsFolder()
-        getImageFromFileManager()
+        getImageFromAssetsFolder()
+//        getImageFromFileManager()
     }
     
     func getImageFromAssetsFolder() {
@@ -74,7 +90,11 @@ class FileManagerViewModel: ObservableObject {
     
     func saveImage() {
         guard let image = image else {return}
-        manager.saveImage(image: image, name: imageName)
+       infoMessage = manager.saveImage(image: image, name: imageName)
+    }
+    
+    func deleteImage() {
+        infoMessage = manager.deleteImage(name: imageName)
     }
 }
 
@@ -94,18 +114,35 @@ struct FileManagerBootcamp: View {
                         .frame(width: 200, height: 200)
                         .clipShape(RoundedRectangle(cornerRadius: 10))
                 }
-                
-                Button {
-                    vm.saveImage()
-                } label: {
-                     Text("Save to FM")
-                        .foregroundStyle(Color.white)
-                        .font(.headline)
-                        .padding()
-                        .padding(.horizontal)
-                        .background(Color.blue)
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                HStack {
+                    Button {
+                        vm.saveImage()
+                    } label: {
+                         Text("Save to FM")
+                            .foregroundStyle(Color.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.blue)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
+                    Button {
+                        vm.deleteImage()
+                    } label: {
+                         Text("Delete from FM")
+                            .foregroundStyle(Color.white)
+                            .font(.headline)
+                            .padding()
+                            .padding(.horizontal)
+                            .background(Color.red)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                    }
                 }
+                Text(vm.infoMessage)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(Color.orange)
+                
 
              
                 Spacer()
